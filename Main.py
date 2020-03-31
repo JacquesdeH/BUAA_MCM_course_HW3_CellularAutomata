1dff9ea3aa8f478d3df1dc4b5241bc0edf607410
JacquesdeH <18231070@buaa.edu.cn>
2020-03-31 23:51:44 +0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+# _*_coding:utf-8_*_
+# Author      : JacquesdeH
+# Create Time : 2020/3/31 23:49
+# Project Name: project
+# File        : Main.py
+# --------------------------------------------------
+import random
+import numpy as np
+
+from Config import Config
+
+if __name__ == '__main__':
+    random.seed(Config.SEED)
+    np.random.seed(Config.SEED)
+
+    raise NotImplementedError('Main')
