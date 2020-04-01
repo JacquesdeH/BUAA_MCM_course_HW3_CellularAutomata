a4c5da46a056ee12f4227de2cfe02204cbc70110
胡雪瑜 <18231094@buaa.edu.cn>
2020-04-01 11:33:39 +0800

    MatrixDtypeToInt

--- InputHandler.py
+++ InputHandler.py
@@ -10,7 +10,7 @@ class InputHandler:
         base = np.array([100, 100, 100])
         size = 15
         img = np.array(Image.open('../ukmap2.jpg'))
-        result = np.zeros((img.shape[0], img.shape[1]))
+        result = np.zeros((img.shape[0], img.shape[1]),dtype=int)
         for i in range(img.shape[0]):
             for j in range(img.shape[1]):
                 if img[i, j, 0] - base[0] < 0:
@@ -18,6 +18,7 @@ class InputHandler:
                 else:
                     result[i, j] = 0
         result = np.pad(result, ((0, 0), (86, 86)), "constant", constant_values=0)
+        print(result.dtype)
         return result.tolist()
 
 inputHandler = InputHandler()
